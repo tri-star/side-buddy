d64f05a9171d76097fbd68ea0136d6e3b630840c
tristar <tristar.2nd@gmail.com>
2023-04-04 08:57:54 +0900

    emotionを追加

---
+++ src/features/chat/panels/SideBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ReactElement } from "react";
+import {css} from "@emotion/react";
+
+
+export const SideBar =  (): ReactElement => {
+
+  const style = css`
+    display: flex;
+  `;
+
+  return (<div>
+
+  </div>);
+};
