5d03b651c1896da613ec4e01a6be3f96ca4c045b
tristar <tristar.2nd@gmail.com>
2023-04-05 02:03:43 +0900

    ESLintが適用できるように対応

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true
+    // "jest/globals": true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'standard-with-typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+    // "plugin:jest/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    tsconfigRootDir: '.',
+    project: [
+      './tsconfig.json',
+      './tsconfig.node.json',
+      './packages/app/tsconfig.json',
+      './packages/app/tsconfig.node.json'
+    ],
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    // "jest",
+    'jsx-a11y',
+    '@typescript-eslint',
+    'react-hooks'
+  ],
+  rules: {
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    'no-console': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: ['*.tsx'],
+      rules: {
+        'react/prop-types': 'off'
+      }
+    }
+    // {
+    //   "files": [
+    //     "src/**/__tests__/**/*.(ts,tsx)",
+    //   ],
+    //   env: {
+    //     "jest/globals": true,
+    //   },
+    //   "extends": [
+    //     "plugin:jest/recommended",
+    //     "plugin:jest/style"
+    //   ],
+    //   "plugins": ["jest"]
+    // }
+  ]
+}
