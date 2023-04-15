1e117f333d6531c0b68c1f9d9ddf18305e5c31b1
tristar <tristar.2nd@gmail.com>
2023-04-16 01:06:52 +0900

    スレッド一覧の外観調整

--- packages/app/src/panels/thread-list/ThreadList.tsx
+++ packages/app/src/panels/thread-list/ThreadList.tsx
@@ -2,7 +2,10 @@ import { listenExtensionMessage } from "@/api/vs-code/listen-extension-message";
 import { sendPanelMessage } from "@/api/vs-code/send-panel-message";
 import { type ExtensionMessage } from "@/domain/extension-message";
 import { type Thread } from "@/domain/thread";
+import { css } from "@emotion/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { type ReactElement, useEffect, useState } from "react";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 
 export function ThreadList(): ReactElement {
 
@@ -22,11 +25,31 @@ export function ThreadList(): ReactElement {
     })
   }, [])
 
+  const lineStyle = css({
+    display: 'flex',
+    cursor: 'pointer',
+    justifyContent: 'start',
+    marginTop: '5px',
+    gap: '5px,',
+    width: '100%',
+    '& > p': {
+      flexGrow: 1,
+      overflow: 'hidden',
+      textOverflow: 'elipsis',
+      whiteSpace: 'nowrap',
+      marginLeft: '5px',
+      width: '100%',
+    }
+  })
+
   return (
     <div>
     {(threads.map(thread => {
       return (
-        <div key={thread.id}>{thread.title}</div>
+        <div key={thread.id} css={lineStyle}>
+          <FontAwesomeIcon icon={faBook} />
+          <p title={thread.title}>{thread.title}</p>
+        </div>
       )
     }))}
     </div>
