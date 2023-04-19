e232e0cb9489ba24ef7510a1a031ea5470e02ecb
tristar <tristar.2nd@gmail.com>
2023-04-20 01:47:22 +0900

    スレッド一覧をVSCodeの外で動作させるための対応(暫定)

--- packages/app/src/thread-list.tsx
+++ packages/app/src/thread-list.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { isVsCodeEnv } from './api/vs-code/vs-code-api'
-import { startExtensionStub } from './api/vs-code/extension-stub'
+import { startExtensionStubThreadList } from './api/vs-code/extension-stub-thread-list'
 import { ThreadList } from './panels/thread-list/ThreadList'
 import './thread-list.css'
 
 if (!isVsCodeEnv()) {
-  void startExtensionStub()
+  void startExtensionStubThreadList()
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
