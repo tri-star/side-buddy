4d936e56553e158c51ba9846faec672e8496c08a
tristar <tristar.2nd@gmail.com>
2023-11-20 00:16:48 +0900

    Refactor thread repository and add thread list migration

--- src/domain/chat.ts
+++ src/domain/chat.ts
@@ -1,5 +1,14 @@
 import zod from 'zod'
 
+export const modelSchema = zod.union([
+  zod.literal('gpt-4-1106-preview'),
+  zod.literal('gpt-4'),
+  zod.literal('gpt-4-32k'),
+  zod.literal('gpt-3.5-turbo-1106'),
+  zod.literal('gpt-3.5-turbo'),
+  zod.literal('gpt-3.5-turbo-16k'),
+])
+
 export const chatMessageSchema = zod.object({
   id: zod.string().min(1),
   role: zod.union([
@@ -7,5 +16,6 @@ export const chatMessageSchema = zod.object({
     zod.literal('assistant'),
     zod.literal('user'),
   ]),
+  model: modelSchema,
   message: zod.string().min(1),
 })
